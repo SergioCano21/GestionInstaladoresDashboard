a7dda641fbabd033c44aa02030452417e70cea69
SergioCano21 <chimo.c.g@hotmail.com>
2026-02-02 02:22:41 -0600

    Refactor useFormHandler to improve state update logic for nested properties

--- src/hooks/useFormHandler.ts
+++ src/hooks/useFormHandler.ts
@@ -6,10 +6,25 @@ export function useFormHandler<T>(setData: React.Dispatch<React.SetStateAction<T
   ) => {
     const { name, value, type } = event.target;
 
-    setData((prev: any) => ({
-      ...prev,
-      [name]: type == 'number' ? (value != '' ? Number(value) : '') : value,
-    }));
+    setData((prev: any) => {
+      const processedValue = type === 'number' ? (value !== '' ? Number(value) : '') : value;
+
+      if (name.includes('.')) {
+        const [parent, child] = name.split('.');
+        return {
+          ...prev,
+          [parent]: {
+            ...prev[parent],
+            [child]: processedValue,
+          },
+        };
+      }
+
+      return {
+        ...prev,
+        [name]: processedValue,
+      };
+    });
   };
   return { handleChange };
 }
